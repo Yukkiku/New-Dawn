eba2056a29a77c1cd5649d9a4ee051e206632e55
Yukkiku <106497277+Yukkiku@users.noreply.github.com>
2020-08-23 16:15:07 +0000

    CHANGELOG++

--- ecommerce/cart/factories.py
+++ ecommerce/cart/factories.py
@@ -3,4 +3,8 @@ import factory
 from ecommerce.cart.models import CartItem
 from ecommerce.cart.models.cart import Cart
 from ecommerce.product.factories import ProductVariationFactory
-from ecommerce.user.factories import UserProfileFactor
+from ecommerce.user.factories import UserProfileFactory
+
+
+class CartFactory(factory.DjangoModelFactory):
+    u
