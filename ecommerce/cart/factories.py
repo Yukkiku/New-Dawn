ccd1a0c81b557ccd8e0858f63a648db55d14e2d6
Yukkiku <106497277+Yukkiku@users.noreply.github.com>
2020-08-23 11:21:28 +0000

    Add coveralls support

--- ecommerce/cart/factories.py
+++ ecommerce/cart/factories.py
@@ -3,3 +3,4 @@ import factory
 from ecommerce.cart.models import CartItem
 from ecommerce.cart.models.cart import Cart
 from ecommerce.product.factories import ProductVariationFactory
+from ecommerce.user.factories import UserProfileFactor
