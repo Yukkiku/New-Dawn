b017cfe3fcb2652ddff893b89ba6685e234d3740
Yukkiku <106497277+Yukkiku@users.noreply.github.com>
2020-09-27 12:43:39 +0000

    add theme

--- ecommerce/cart/migrations/0002_auto_20200829_2252.py
+++ ecommerce/cart/migrations/0002_auto_20200829_2252.py
@@ -4,4 +4,8 @@ from django.db import migrations, models
 import django.db.models.deletion
 
 
-class Migration(migrations.Migratio
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('user', '0003_customer'),
+      
