b29497947062fa28d03e7adcb67c08adf8ba70ae
Yukkiku <106497277+Yukkiku@users.noreply.github.com>
2020-10-23 09:32:02 +0000

    Changed repo name

--- ecommerce/cart/models/cart.py
+++ ecommerce/cart/models/cart.py
@@ -2,4 +2,7 @@ from django.db import models
 
 from ecommerce.cart.models.cart_item import CartItem
 from ecommerce.product.models.product_variation import ProductVariation
-f
+from ecommerce.user.models import UserProfile
+
+
+clas
