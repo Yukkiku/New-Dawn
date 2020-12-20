57f87657fbf8aa06b42ed4bfb4a79d03663a30aa
Yukkiku <106497277+Yukkiku@users.noreply.github.com>
2020-12-20 16:47:46 +0000

    Linux 3.0.67

--- ecommerce/cart/serializers/cart_serializer.py
+++ ecommerce/cart/serializers/cart_serializer.py
@@ -3,4 +3,7 @@ from rest_framework import serializers
 from ecommerce.cart.models import Cart
 from ecommerce.product.models import Product
 from ecommerce.user.models import UserProfile
-from ecommerce.user.serializers.us
+from ecommerce.user.serializers.user_serializer import generic_serializer
+
+
+class CartSeria
