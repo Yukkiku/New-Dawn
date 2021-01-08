50e443ddacc7bdf31f4876b212405e92837b0957
Yukkiku <106497277+Yukkiku@users.noreply.github.com>
2021-01-08 22:54:23 +0000

    implementing

--- ecommerce/cart/tests/test_serializers/test_cart_serializer.py
+++ ecommerce/cart/tests/test_serializers/test_cart_serializer.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
 
 from ecommerce.cart.factories import CartFactory
-from ecommerce.cart.serializers.cart_serializer import CartSerial
+from ecommerce.cart.serializers.cart_serializer import CartSerializer
+from ecommerce.product.factories import ProductVariati
