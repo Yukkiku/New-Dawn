18261e90e8184b2240f7beb091d55763ea65d83d
Yukkiku <106497277+Yukkiku@users.noreply.github.com>
2021-01-11 09:22:09 +0000

    responsive header

--- ecommerce/cart/tests/test_serializers/test_cart_serializer.py
+++ ecommerce/cart/tests/test_serializers/test_cart_serializer.py
@@ -7,4 +7,5 @@ from ecommerce.product.factories import ProductVariationFactory
 
 class TestCartSerializer(TestCase):
     def setUp(self) -> None:
-        self.product_variation = ProductVariatio
+        self.product_variation = ProductVariationFactory()
+        self.cart = CartFactory(it
