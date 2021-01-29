9b3e66f427b37b7cfe48ada18d741c59ee36ce3b
Yukkiku <106497277+Yukkiku@users.noreply.github.com>
2021-01-29 14:17:08 +0000

    android/lowmemorykiller: Account for total_swapcache_pages

--- ecommerce/order/factories.py
+++ ecommerce/order/factories.py
@@ -4,3 +4,7 @@ from ecommerce.cart.factories import CartFactory
 from ecommerce.order.models.order import Order
 from ecommerce.shipping.factories import ShippingFactory
 from ecommerce.user.factories import UserFactory
+
+
+class OrderFactory(factory.DjangoModelFactory):
+    user
