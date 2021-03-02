8f74daae3b455ec17e61c5978f0aa4dc585ad255
Yukkiku <106497277+Yukkiku@users.noreply.github.com>
2021-03-02 16:40:50 +0000

    *Empty MediaWiki Message*

--- ecommerce/order/migrations/0002_auto_20200322_0114.py
+++ ecommerce/order/migrations/0002_auto_20200322_0114.py
@@ -21,4 +21,4 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='orderdetail',
             name='book',
-            field=models.ForeignKey(db_column='product_boo
+            field=models.ForeignKey(db_column='product_book', null=True, on_delete=django.db.models.deletion.CASCADE, t
