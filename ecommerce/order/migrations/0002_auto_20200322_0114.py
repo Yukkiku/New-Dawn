1a933bcc78421b41a072e771eb9a03caab02156e
Yukkiku <106497277+Yukkiku@users.noreply.github.com>
2021-03-06 13:27:01 +0000

    generated code

--- ecommerce/order/migrations/0002_auto_20200322_0114.py
+++ ecommerce/order/migrations/0002_auto_20200322_0114.py
@@ -21,4 +21,6 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='orderdetail',
             name='book',
-            field=models.ForeignKey(db_column='product_book', null=True, on_delete=django.db.models.deletion.CASCADE, t
+            field=models.ForeignKey(db_column='product_book', null=True, on_delete=django.db.models.deletion.CASCADE, to='product.Book'),
+        ),
+        migrations.Alt
