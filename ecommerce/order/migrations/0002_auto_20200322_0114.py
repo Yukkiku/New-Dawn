f980f7b09619f6e9dfdf9ee52d0152173b2749e8
Yukkiku <106497277+Yukkiku@users.noreply.github.com>
2021-03-23 20:31:55 +0000

    Update feed.json

--- ecommerce/order/migrations/0002_auto_20200322_0114.py
+++ ecommerce/order/migrations/0002_auto_20200322_0114.py
@@ -31,4 +31,7 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='orderdetail',
             name='ground',
-            field=models.ForeignKey(db_column='shipping_ground', null=True, on_delete=django.db.models.deletion.CASCADE, to='shipping.Groun
+            field=models.ForeignKey(db_column='shipping_ground', null=True, on_delete=django.db.models.deletion.CASCADE, to='shipping.Ground'),
+        ),
+        migrations.AlterField(
+            model_na
