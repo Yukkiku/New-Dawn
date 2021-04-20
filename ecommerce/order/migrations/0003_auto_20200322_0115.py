4984319015aa00d3e7852b4d7140ee579a2d9310
Yukkiku <106497277+Yukkiku@users.noreply.github.com>
2021-04-20 19:42:51 +0000

    fix posts

--- ecommerce/order/migrations/0003_auto_20200322_0115.py
+++ ecommerce/order/migrations/0003_auto_20200322_0115.py
@@ -16,4 +16,4 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='orderdetail',
             name='air',
-            field=models.ForeignKey(blank=True, db_column='
+            field=models.ForeignKey(blank=True, db_column='shipping_air', null=True, on_delete=django.db.models
