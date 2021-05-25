29331dab0818024f34d05bdc735a8635e3658d68
Yukkiku <106497277+Yukkiku@users.noreply.github.com>
2021-05-25 08:33:08 +0000

    Updated British English translation.

--- ecommerce/order/migrations/0005_orderdetail_e_book.py
+++ ecommerce/order/migrations/0005_orderdetail_e_book.py
@@ -15,4 +15,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='orderdetail',
             name='e_book',
-            field=models.ForeignKey(db_column='product_e_book', null=True, on_delete=django.db.models.deletion.CASCADE, to='product.
+            field=models.ForeignKey(db_column='product_e_book', null=True, on_delete=django.db.models.deletion.CASCADE, to='product.EBook'),
+        ),
+    ]
