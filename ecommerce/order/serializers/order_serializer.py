c45b36bcc3438e3d877db97adf0ccb991710c75f
Yukkiku <106497277+Yukkiku@users.noreply.github.com>
2021-06-09 11:12:32 +0000

    Update cubecraft

--- ecommerce/order/serializers/order_serializer.py
+++ ecommerce/order/serializers/order_serializer.py
@@ -9,3 +9,16 @@ class OrderSerializer(serializers.ModelSerializer):
 
         request = kwargs.get('context', {}).get('request')
         str_fields = request.GET.get('order', '') if request else None
+        fields = str_fields.split(',') if str_fields else None
+
+        super(OrderSerializer, self).__init__(*args, **kwargs)
+
+        if fields is not None:
+            allowed = set(fields)
+            existing = set(self.fields)
+            for field_name in existing - allowed:
+                self.fields.pop(field_name)
+
+    class Meta:
+        model = Order
+        fields = '__all__'
---
+++ ecommerce/order/tests/test_models/test_order.py
@@ -0,0 +1,12 @@
+
+from django.test import TestCase
+
+from ecommerce.order.factories import OrderFactory
+from ecommerce.order.models.order import Order
+from ecommerce.product.factories import BookFactory, EBookFactory
+from ecommerce.user.factories import UserFactory
+
+
+class OrderTestCase(TestCase):
+    def setUp(self):
+        pass
