8d89d4d4d05cba42fb141d0fdfa1e00b78cae472
Yukkiku <106497277+Yukkiku@users.noreply.github.com>
2021-07-19 12:22:21 +0000

    Inital commit.

--- ecommerce/product/migrations/0001_initial.py
+++ ecommerce/product/migrations/0001_initial.py
@@ -15,4 +15,5 @@ class Migration(migrations.Migration):
             name='Book',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=
+                ('name', models.CharField(max_length=30)),
+                ('price', models.PositiveIntegerFi
