4dcd1038d95dbc3f84228f5360bb5f259020bd95
Yukkiku <106497277+Yukkiku@users.noreply.github.com>
2021-07-19 10:42:22 +0000

    init jekyll

--- ecommerce/product/migrations/0001_initial.py
+++ ecommerce/product/migrations/0001_initial.py
@@ -14,4 +14,5 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Book',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_nam
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('name', models.CharField(max_length=
