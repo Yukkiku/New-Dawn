18dbf74e9ac9e1d46ef490b9ee46f43fd07e04a9
Yukkiku <106497277+Yukkiku@users.noreply.github.com>
2021-08-07 21:50:42 +0000

    Update development and testing dependencies

--- ecommerce/product/migrations/0002_auto_20200322_0126.py
+++ ecommerce/product/migrations/0002_auto_20200322_0126.py
@@ -13,4 +13,7 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='book',
             name='name',
-            field=models.CharField(blank=True, max_length=30
+            field=models.CharField(blank=True, max_length=30, null=True),
+        ),
+        migrations.AlterField(
+            m
