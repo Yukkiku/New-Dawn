bf6fb7f39f15fa4aa2abaf73163dcf78b6a71c1b
Yukkiku <106497277+Yukkiku@users.noreply.github.com>
2021-09-04 06:16:41 +0000

    change project

--- ecommerce/product/migrations/0003_auto_20200425_2142.py
+++ ecommerce/product/migrations/0003_auto_20200425_2142.py
@@ -21,4 +21,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='ebook',
             name='order',
-            field=models.ForeignKey(default=1, on_delete=django.db.models.deletion.CASCADE, related_name='ebooks', to='ord
+            field=models.ForeignKey(default=1, on_delete=django.db.models.deletion.CASCADE, related_name='ebooks', to='order.Order'),
+            preserve_default=False,
+       
