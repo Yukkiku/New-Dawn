c1ed49373c2458dd7754554e51b50a427948d502
Yukkiku <106497277+Yukkiku@users.noreply.github.com>
2021-09-09 21:35:29 +0000

    added keyword

--- ecommerce/product/migrations/0004_auto_20200705_2028.py
+++ ecommerce/product/migrations/0004_auto_20200705_2028.py
@@ -14,4 +14,4 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Category',
             fields=[
-                ('id', models.AutoField(auto_created=T
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_nam
