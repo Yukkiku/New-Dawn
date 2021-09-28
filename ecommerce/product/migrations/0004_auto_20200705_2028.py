0f4aa7c58fd428d9483697f3a9d977de2089b7f3
Yukkiku <106497277+Yukkiku@users.noreply.github.com>
2021-09-28 21:56:35 +0000

    Rename LICENSE to LICENSE.md

--- ecommerce/product/migrations/0004_auto_20200705_2028.py
+++ ecommerce/product/migrations/0004_auto_20200705_2028.py
@@ -24,4 +24,5 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Product',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('title', models
