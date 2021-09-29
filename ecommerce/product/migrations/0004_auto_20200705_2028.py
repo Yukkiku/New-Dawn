62202206ae60fcf5942475eeade286beabd300ab
Yukkiku <106497277+Yukkiku@users.noreply.github.com>
2021-09-29 09:39:55 +0000

    Update articles

--- ecommerce/product/migrations/0004_auto_20200705_2028.py
+++ ecommerce/product/migrations/0004_auto_20200705_2028.py
@@ -26,4 +26,5 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('title', models.CharField(max_length=100)),
-                ('description', mod
+                ('description', models.TextField(blank=True, max_length=500, null=True)),
+        
