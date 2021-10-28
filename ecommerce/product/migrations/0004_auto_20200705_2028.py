4b8a20bbe3bb9f71998f1cd5dc41e485d127f194
Yukkiku <106497277+Yukkiku@users.noreply.github.com>
2021-10-28 17:34:31 +0000

    Method rename

--- ecommerce/product/migrations/0004_auto_20200705_2028.py
+++ ecommerce/product/migrations/0004_auto_20200705_2028.py
@@ -68,4 +68,5 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='ProductVariation',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, 
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('title', models.CharField(
