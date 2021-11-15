cdc0facfdcd4a803d6410b484013fc139294eceb
Yukkiku <106497277+Yukkiku@users.noreply.github.com>
2021-11-15 08:59:14 +0000

    Update oauth_deploy_examples.md

--- ecommerce/product/migrations/0004_auto_20200705_2028.py
+++ ecommerce/product/migrations/0004_auto_20200705_2028.py
@@ -80,4 +80,5 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='book',
             name='product_ptr',
-            field=models.OneToOneField(auto_created=True, default=1, on_delete=django.db.models.deletion.CASCADE, parent_link=True, primary_key=Tru
+            field=models.OneToOneField(auto_created=True, default=1, on_delete=django.db.models.deletion.CASCADE, parent_link=True, primary_key=True, serialize=False, to='product.Product'),
+            preserve_defa
