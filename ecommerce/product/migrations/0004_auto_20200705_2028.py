951f0cce5443c6079c3738f7eed31579fc54e5c0
Yukkiku <106497277+Yukkiku@users.noreply.github.com>
2021-11-15 22:54:18 +0000

    at

--- ecommerce/product/migrations/0004_auto_20200705_2028.py
+++ ecommerce/product/migrations/0004_auto_20200705_2028.py
@@ -86,4 +86,4 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='ebook',
             name='product_ptr',
-      
+            field=models.OneToOneField(auto_created=True, default=1
