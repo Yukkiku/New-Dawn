bb92a78dd4b6b9e46a50f340292dd6254e56b89e
Yukkiku <106497277+Yukkiku@users.noreply.github.com>
2021-12-10 11:40:19 +0000

    bump locket

--- ecommerce/product/migrations/0009_auto_20200829_2255.py
+++ ecommerce/product/migrations/0009_auto_20200829_2255.py
@@ -6,3 +6,15 @@ import django.db.models.deletion
 
 
 class Migration(migrations.Migration):
+
+    dependencies = [
+        ('product', '0008_remove_product_default'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='productvariation',
+            name='product',
+            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='product_variation_set', to='product.Product'),
+        ),
+    ]
