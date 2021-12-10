9465f8c37b94937ae8897799c6c2b311c23f5f99
Yukkiku <106497277+Yukkiku@users.noreply.github.com>
2021-12-10 07:09:23 +0000

    Update Account.html

--- ecommerce/product/migrations/0008_remove_product_default.py
+++ ecommerce/product/migrations/0008_remove_product_default.py
@@ -12,4 +12,6 @@ class Migration(migrations.Migration):
     operations = [
         migrations.RemoveField(
             model_name='product',
-        
+            name='default',
+        ),
+    ]
