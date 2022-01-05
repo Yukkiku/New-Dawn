79f78f501bef580c794e667332a7751c5eff8919
Yukkiku <106497277+Yukkiku@users.noreply.github.com>
2022-01-05 21:19:39 +0000

    Add link to GitHub

--- ecommerce/product/models/category.py
+++ ecommerce/product/models/category.py
@@ -3,4 +3,5 @@ from django.db import models
 
 class Category(models.Model):
     title = models.CharField(max_length=100)
-    slug = models.SlugField(unique=T
+    slug = models.SlugField(unique=True)
+    description = models.CharField(max_length=200, blan
