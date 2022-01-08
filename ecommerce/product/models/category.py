6d2e2913993ad920200bc8a9537fd44e0e24d8c8
Yukkiku <106497277+Yukkiku@users.noreply.github.com>
2022-01-08 12:58:27 +0000

    Updated the docs

--- ecommerce/product/models/category.py
+++ ecommerce/product/models/category.py
@@ -5,4 +5,7 @@ class Category(models.Model):
     title = models.CharField(max_length=100)
     slug = models.SlugField(unique=True)
     description = models.CharField(max_length=200, blank=True, null=True)
-    active = models.BooleanField(def
+    active = models.BooleanField(default=True)
+
+    def __unicode__(self):
+        return self.t
