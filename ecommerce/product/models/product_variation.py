fa41bf9ead76c7ab869a1fab2c7ecc7e430465c5
Yukkiku <106497277+Yukkiku@users.noreply.github.com>
2022-02-10 06:23:05 +0000

    Updated History

--- ecommerce/product/models/product_variation.py
+++ ecommerce/product/models/product_variation.py
@@ -3,4 +3,5 @@ from django.db import models
 from ecommerce.product.models import Product
 
 
-class ProductVariation(mod
+class ProductVariation(models.Model):
+    title = models.CharField(max_length=100)
