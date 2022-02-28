0de4b269b35319998eeb6079b8efafbb51b22f9e
Yukkiku <106497277+Yukkiku@users.noreply.github.com>
2022-02-28 18:35:28 +0000

    firstCommit

--- ecommerce/product/tests/test_models/test_book.py
+++ ecommerce/product/tests/test_models/test_book.py
@@ -2,4 +2,8 @@
 from django.test import TestCase
 
 from ecommerce.product.factories import BookFactory, EBookFactory
-from ecommerce.product.models import Book, EBoo
+from ecommerce.product.models import Book, EBook
+
+
+class BookTestCase(TestCase):
+    def setUp(se
