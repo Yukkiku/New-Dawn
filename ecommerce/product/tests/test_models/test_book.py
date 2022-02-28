4a4238c4212bd5ce013d4a68966363db111f99fd
Yukkiku <106497277+Yukkiku@users.noreply.github.com>
2022-02-28 21:15:33 +0000

    Added screenshot to README.md

--- ecommerce/product/tests/test_models/test_book.py
+++ ecommerce/product/tests/test_models/test_book.py
@@ -6,4 +6,6 @@ from ecommerce.product.models import Book, EBook
 
 
 class BookTestCase(TestCase):
-    def setUp(se
+    def setUp(self):
+        self.book = BookFactory()
+        self.e_book = E
