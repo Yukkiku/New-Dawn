5c0c367100463a01ee4d8d09a5e30985a1ae340b
Yukkiku <106497277+Yukkiku@users.noreply.github.com>
2022-03-07 18:03:50 +0000

    updated _config.yml

--- ecommerce/product/tests/test_models/test_category.py
+++ ecommerce/product/tests/test_models/test_category.py
@@ -1,3 +1,7 @@
 from unittest import TestCase
 
-from ecommerce.product.factories import ProductFactory, CategoryFact
+from ecommerce.product.factories import ProductFactory, CategoryFactory
+from ecommerce.product.models import Category
+
+
+class TestCatego
