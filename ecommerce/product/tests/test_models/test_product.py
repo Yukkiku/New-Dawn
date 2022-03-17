e7ac09cd904df5ffae75562dfc61a7620ad88946
Yukkiku <106497277+Yukkiku@users.noreply.github.com>
2022-03-17 14:19:52 +0000

    Longer timeout

--- ecommerce/product/tests/test_models/test_product.py
+++ ecommerce/product/tests/test_models/test_product.py
@@ -4,4 +4,5 @@ from ecommerce.product.factories import ProductFactory, CategoryFactory
 from ecommerce.product.models import Product, ProductVariation
 
 
-class
+class TestProductVariationCase(TestCase):
+    def setUp(self
