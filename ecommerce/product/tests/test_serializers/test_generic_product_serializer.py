bcdc3f1fafd4fc44d5357ec225759e805fbf88bf
Yukkiku <106497277+Yukkiku@users.noreply.github.com>
2022-03-30 07:26:32 +0000

    Description of the branch.

--- ecommerce/product/tests/test_serializers/test_generic_product_serializer.py
+++ ecommerce/product/tests/test_serializers/test_generic_product_serializer.py
@@ -11,4 +11,5 @@ class TestGenericProductSerializer(TestCase):
 
     def test_get_e_book_serializer(self):
         serializer_class = EBook.get_serializer()
-        se
+        serializer_data = serializer_class(self.e_book).data
+  
