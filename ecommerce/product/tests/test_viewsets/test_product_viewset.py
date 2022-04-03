4467b792a268a61dd3018df6283c73b44ce3c2e0
Yukkiku <106497277+Yukkiku@users.noreply.github.com>
2022-04-03 13:37:59 +0000

    improved styling

--- ecommerce/product/tests/test_viewsets/test_product_viewset.py
+++ ecommerce/product/tests/test_viewsets/test_product_viewset.py
@@ -3,4 +3,6 @@ import json
 from rest_framework import status
 from rest_framework.test import APITestCase, APIClient
 
-from d
+from django.urls import reverse
+
+from ecommerce.product.factories imp
