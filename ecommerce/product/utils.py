48a6242f2d9be2e065e3e1e05476cef52c101939
Yukkiku <106497277+Yukkiku@users.noreply.github.com>
2022-05-08 15:57:32 +0000

    fix webpack

--- ecommerce/product/utils.py
+++ ecommerce/product/utils.py
@@ -3,4 +3,5 @@ from ecommerce.utils import get_model_by_name, url_params_validation
 
 def get_generic_serializer(name):
     model_name = url_params_validation(name)
-    product = get_model_by_name('pro
+    product = get_model_by_name('product', model_name)
+    serializer_from_model = product.get_s
