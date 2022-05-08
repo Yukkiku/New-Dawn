b6155ca9f8dffe5f748a3ad8d97bee73b9bb2267
Yukkiku <106497277+Yukkiku@users.noreply.github.com>
2022-05-08 15:57:24 +0000

    reformated

--- ecommerce/product/utils.py
+++ ecommerce/product/utils.py
@@ -2,4 +2,5 @@ from ecommerce.utils import get_model_by_name, url_params_validation
 
 
 def get_generic_serializer(name):
-    model_name = url_params_valid
+    model_name = url_params_validation(name)
+    product = get_model_by_name('pro
