5e19311c28ed3bb053dd11145adf896d01d3e531
Yukkiku <106497277+Yukkiku@users.noreply.github.com>
2022-05-09 10:14:58 +0000

    Update .Rbuildignore

--- ecommerce/product/utils.py
+++ ecommerce/product/utils.py
@@ -4,4 +4,5 @@ from ecommerce.utils import get_model_by_name, url_params_validation
 def get_generic_serializer(name):
     model_name = url_params_validation(name)
     product = get_model_by_name('product', model_name)
-    serializer_from_model = product.get_s
+    serializer_from_model = product.get_serializer()
+    return serializer_from_model
