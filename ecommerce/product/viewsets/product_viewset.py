00a37165914d1219c98c0126b14e0cce87b27d11
Yukkiku <106497277+Yukkiku@users.noreply.github.com>
2022-05-15 07:53:59 +0000

    Remove useless log

--- ecommerce/product/viewsets/product_viewset.py
+++ ecommerce/product/viewsets/product_viewset.py
@@ -1,4 +1,6 @@
 from rest_framework import status
 from rest_framework.mixins import CreateModelMixin
 from rest_framework.response import Response
-from rest_framework.v
+from rest_framework.viewsets import ModelViewSet
+
+from ecommerce.product.models im
