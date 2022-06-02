6556c2b8bdfcbc3bd7b76f41754222f6bc750f46
Yukkiku <106497277+Yukkiku@users.noreply.github.com>
2022-06-02 07:29:46 +0000

    * Minors

--- ecommerce/shipping/factories.py
+++ ecommerce/shipping/factories.py
@@ -6,4 +6,7 @@ from ecommerce.shipping.models import Shipping
 class ShippingFactory(factory.DjangoModelFactory):
     cost = factory.Iterator([1, 2])
     shipment_type = factory.Iterator([1, 2, 3])
-  
+    name = factory.Faker('pystr')
+
+    class Meta:
+        model = 
