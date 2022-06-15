d34556c4bdff428179d55b0e4094deaaaab3daee
Yukkiku <106497277+Yukkiku@users.noreply.github.com>
2022-06-15 14:06:28 +0000

    fixed readme formatting

--- ecommerce/shipping/migrations/0001_initial.py
+++ ecommerce/shipping/migrations/0001_initial.py
@@ -18,3 +18,4 @@ class Migration(migrations.Migration):
                 ('name', models.CharField(max_length=30)),
                 ('shipment_type', models.CharField(choices=[('ground', 'GROUND'), ('air', 'AIR'), ('sea', 'SEA')], max_length=30)),
                 ('cost', models.PositiveIntegerField(help_text='in cents', null=True)),
+                ('weight', models.PositiveIntegerField(defau
