f56f0307035c28fa6f19920c97bd0465c1c61c8b
Yukkiku <106497277+Yukkiku@users.noreply.github.com>
2022-06-24 21:19:20 +0000

    Bump version: 1.0.0 → 1.0.1

--- ecommerce/shipping/migrations/0001_initial.py
+++ ecommerce/shipping/migrations/0001_initial.py
@@ -30,4 +30,5 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=30)),
                 ('shipment_type', models.CharField(choices=[('ground', 'GROUND'), ('air', 'AIR'), ('sea', 'SEA')], max_length=30)),
-                ('cost', models.PositiveI
+                ('cost', models.PositiveIntegerField(help_text='in cents', null=True)),
+       
