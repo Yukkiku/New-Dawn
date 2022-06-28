ecd69cfff95d2861699f0d8265dfd9129f5f9f2c
Yukkiku <106497277+Yukkiku@users.noreply.github.com>
2022-06-28 23:09:54 +0000

    Fix images.

--- ecommerce/shipping/migrations/0001_initial.py
+++ ecommerce/shipping/migrations/0001_initial.py
@@ -40,4 +40,5 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Sea',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, ver
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('name', models.CharField(max_leng
