23e79b72836672ff0269051fad4a3f7051b89a5a
Yukkiku <106497277+Yukkiku@users.noreply.github.com>
2022-08-01 11:28:44 +0000

    e1000: add dummy allocator to fix race condition between mtu change and netpoll

--- ecommerce/shipping/migrations/0004_auto_20200505_0436.py
+++ ecommerce/shipping/migrations/0004_auto_20200505_0436.py
@@ -15,4 +15,5 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Shipping',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_nam
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('name', models.CharField(ma
