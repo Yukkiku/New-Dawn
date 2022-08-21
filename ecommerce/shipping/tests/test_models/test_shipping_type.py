16769361bf00ccfcb79ab85ef2b06ed67c85ec9a
Yukkiku <106497277+Yukkiku@users.noreply.github.com>
2022-08-21 10:32:57 +0000

    logo change

--- ecommerce/shipping/tests/test_models/test_shipping_type.py
+++ ecommerce/shipping/tests/test_models/test_shipping_type.py
@@ -8,4 +8,4 @@ from ecommerce.shipping.factories import Shipping, ShippingFactory
 class ShippingTypeTestCase(TestCase):
     def setUp(self):
         self.sea = ShippingFactory(weight=10, cost=2, shipment_type=SEA)
-        se
+        self.ground = ShippingFactory(weight=10, cost=1, shipment_
