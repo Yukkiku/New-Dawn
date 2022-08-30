9cd21764e7b41363507efc1a30dfb96df223610a
Yukkiku <106497277+Yukkiku@users.noreply.github.com>
2022-08-30 13:35:38 +0000

    edit Readme

--- ecommerce/shipping/tests/test_serializers/test_shipping_serializer.py
+++ ecommerce/shipping/tests/test_serializers/test_shipping_serializer.py
@@ -1,4 +1,8 @@
 from django.test import TestCase
 
 from ecommerce.shipping.factories import ShippingFactory
-from ecommerce.shipping.serializers.shipping_serializer import Shipp
+from ecommerce.shipping.serializers.shipping_serializer import ShippingSerializer
+
+
+class TestShippingSerializer(TestCase):
+    def 
