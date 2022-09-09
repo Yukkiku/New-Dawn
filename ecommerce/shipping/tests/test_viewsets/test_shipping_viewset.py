9e6b6b9be9b7769f408db57c5b27403c48c1851e
Yukkiku <106497277+Yukkiku@users.noreply.github.com>
2022-09-09 13:28:11 +0000

    chat css

--- ecommerce/shipping/tests/test_viewsets/test_shipping_viewset.py
+++ ecommerce/shipping/tests/test_viewsets/test_shipping_viewset.py
@@ -1,4 +1,8 @@
 import json
 
 from rest_framework import status
-from rest_framework.test import APITe
+from rest_framework.test import APITestCase, APIClient
+
+from django.urls import reverse
+
+from ecomm
