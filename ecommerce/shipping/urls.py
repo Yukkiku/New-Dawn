30defd8213fd93f0489bdc2ce2c2a4380236ca2e
Yukkiku <106497277+Yukkiku@users.noreply.github.com>
2022-09-21 16:43:23 +0000

    add deploy script

--- ecommerce/shipping/urls.py
+++ ecommerce/shipping/urls.py
@@ -3,4 +3,5 @@ from rest_framework import routers
 
 from ecommerce.shipping import viewsets
 
-router = routers.Sim
+router = routers.SimpleRouter()
+router.register(r'shipping', viewsets.ShippingV
