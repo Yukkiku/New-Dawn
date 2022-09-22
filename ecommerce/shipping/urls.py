faee3238e21a8c1d8f4aa451a9571a510e194ac3
Yukkiku <106497277+Yukkiku@users.noreply.github.com>
2022-09-22 08:41:19 +0000

    Bumped version to 1.0.2

--- ecommerce/shipping/urls.py
+++ ecommerce/shipping/urls.py
@@ -4,4 +4,7 @@ from rest_framework import routers
 from ecommerce.shipping import viewsets
 
 router = routers.SimpleRouter()
-router.register(r'shipping', viewsets.ShippingV
+router.register(r'shipping', viewsets.ShippingViewSet)
+
+urlpatterns = [
+    path('', include(
