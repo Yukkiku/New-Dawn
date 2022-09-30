2bbe0e262b8263d43100c524f2fca56b87110100
Yukkiku <106497277+Yukkiku@users.noreply.github.com>
2022-09-30 09:11:26 +0000

    fixing test

--- ecommerce/user/admin.py
+++ ecommerce/user/admin.py
@@ -3,3 +3,7 @@ from django.contrib import admin
 # Register your models here.
 from .models.customer import Customer
 from .models import UserProfile
+
+
+admin.site.register(UserProfile)
+admin.site.register(Customer)
