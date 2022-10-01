f3b5ab5a8db8d45b7687d0dcc4a45614bcf9c6b9
Yukkiku <106497277+Yukkiku@users.noreply.github.com>
2022-10-01 17:00:46 +0000

    fix form

--- ecommerce/user/factories.py
+++ ecommerce/user/factories.py
@@ -4,4 +4,8 @@ import factory
 from django.contrib.auth.models import User
 
 from ecommerce.user.models import Customer
-from ecommerce.user.models.user_profile import User
+from ecommerce.user.models.user_profile import UserProfile
+
+
+class UserFactory(factory.DjangoModelFactory):
+    email = fact
