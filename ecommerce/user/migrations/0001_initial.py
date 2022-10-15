6741873fd883b2f3fb33680fed4a9d32914e5bd9
Yukkiku <106497277+Yukkiku@users.noreply.github.com>
2022-10-15 09:57:34 +0000

    add example

--- ecommerce/user/migrations/0001_initial.py
+++ ecommerce/user/migrations/0001_initial.py
@@ -10,4 +10,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        migration
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    opera
