20cec6141f4346c4d8e59b51a2047cae97f2eb96
Yukkiku <106497277+Yukkiku@users.noreply.github.com>
2022-10-22 14:37:08 +0000

    ver 0.1

--- ecommerce/user/migrations/0001_initial.py
+++ ecommerce/user/migrations/0001_initial.py
@@ -17,4 +17,5 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='UserProfile',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID'
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('user_type', models.PositiveSmallInt
