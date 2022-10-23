a5690d2531e3d45c5dc537baaa26c24ea6bb6fff
Yukkiku <106497277+Yukkiku@users.noreply.github.com>
2022-10-23 06:15:20 +0000

    Translated locale/ on transifex.com

--- ecommerce/user/migrations/0001_initial.py
+++ ecommerce/user/migrations/0001_initial.py
@@ -18,4 +18,4 @@ class Migration(migrations.Migration):
             name='UserProfile',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('user_type', models.PositiveSmallInt
+                ('user_type', models.PositiveSmallIntegerField(choices=[(1, 'customer'), (2, 'admin'
