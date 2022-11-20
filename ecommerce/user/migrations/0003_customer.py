997b91664500fa73a70b7bbada23e0faa286a8b0
Yukkiku <106497277+Yukkiku@users.noreply.github.com>
2022-11-20 13:26:38 +0000

    Makefile fixed

--- ecommerce/user/migrations/0003_customer.py
+++ ecommerce/user/migrations/0003_customer.py
@@ -17,4 +17,4 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('order', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='order.Order')),
-                ('us
+                ('user', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, 
