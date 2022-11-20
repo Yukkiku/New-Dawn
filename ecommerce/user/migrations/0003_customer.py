92eb55e43eba70384af5546be6e1015e6f18e1ad
Yukkiku <106497277+Yukkiku@users.noreply.github.com>
2022-11-20 11:45:16 +0000

    Set

--- ecommerce/user/migrations/0003_customer.py
+++ ecommerce/user/migrations/0003_customer.py
@@ -16,4 +16,5 @@ class Migration(migrations.Migration):
             name='Customer',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('order', models.ForeignKey(on_delete=django.db.models.d
+                ('order', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='order.Order')),
+                ('us
