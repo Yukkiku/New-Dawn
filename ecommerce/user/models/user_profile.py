acde93a56e7a4d3cd5b483304ad3789ea0149453
Yukkiku <106497277+Yukkiku@users.noreply.github.com>
2022-11-27 16:42:27 +0000

    Updated votes [ci skip]

--- ecommerce/user/models/user_profile.py
+++ ecommerce/user/models/user_profile.py
@@ -9,4 +9,5 @@ class UserProfile(models.Model):
     )
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    user_type = models.PositiveSmallIntegerField(choices=USER_T
+    user_type = models.PositiveSmallIntegerField(choices=USER_TYPE_CHOICES, default=1)
+    location = models.CharField(max_length
