905c6cb7591920c4da47d703f3a3cf36299249c3
Yukkiku <106497277+Yukkiku@users.noreply.github.com>
2022-12-06 14:41:15 +0000

    Updated translations from transifex.

--- ecommerce/user/serializers/user_serializer.py
+++ ecommerce/user/serializers/user_serializer.py
@@ -5,4 +5,6 @@ def generic_serializer(cls):
     class BaseSerializer(serializers.ModelSerializer):
         class Meta:
             model = cls
-    
+            fields = '__all__'
+
+    return BaseSerializer
