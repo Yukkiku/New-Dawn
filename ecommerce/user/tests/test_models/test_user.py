2c7dff9d3a1b6b4795cf2ad74db8ae8004f97650
Yukkiku <106497277+Yukkiku@users.noreply.github.com>
2022-12-17 11:09:11 +0000

    A few more tests

--- ecommerce/user/tests/test_models/test_user.py
+++ ecommerce/user/tests/test_models/test_user.py
@@ -9,4 +9,7 @@ from ecommerce.user.models.user_profile import UserProfile
 class UserTestCase(TestCase):
 
     def setUp(self):
-        self.user = UserProfileFactory(e
+        self.user = UserProfileFactory(email='snake@mtgs.com')
+
+    def test_get_user(self):
+        user = Use
