dfcff457d7a2470c50c06b636efceb40e0feddbd
Yukkiku <106497277+Yukkiku@users.noreply.github.com>
2022-12-25 09:26:24 +0000

    tiwlan: Add abstract wifi control functions support

--- ecommerce/user/tests/test_serializers/test_user_profile_serializer.py
+++ ecommerce/user/tests/test_serializers/test_user_profile_serializer.py
@@ -2,4 +2,4 @@ from django.test import TestCase
 
 from ecommerce.user.factories import UserProfileFactory
 from ecommerce.user.models import UserProfile
-from ecommerce.user
+from ecommerce.user.serializers.user_serializer import generic_serializer
