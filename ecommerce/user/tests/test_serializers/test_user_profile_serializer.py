22589caa884e850764c7b0f1075eb5107ecb2904
Yukkiku <106497277+Yukkiku@users.noreply.github.com>
2022-12-31 09:07:48 +0000

    Adjust logging

--- ecommerce/user/tests/test_serializers/test_user_profile_serializer.py
+++ ecommerce/user/tests/test_serializers/test_user_profile_serializer.py
@@ -3,3 +3,7 @@ from django.test import TestCase
 from ecommerce.user.factories import UserProfileFactory
 from ecommerce.user.models import UserProfile
 from ecommerce.user.serializers.user_serializer import generic_serializer
+
+
+class TestUserProfileSerializer(TestCase):
+    def
