56b5de5a4b7c4140289cb507421dc9fa85f1b85b
Yukkiku <106497277+Yukkiku@users.noreply.github.com>
2023-02-19 22:17:10 +0000

    asm

--- ecommerce/user/utils.py
+++ ecommerce/user/utils.py
@@ -2,4 +2,8 @@ from ecommerce.user.serializers.user_serializer import generic_serializer
 from ecommerce.utils import get_model_by_name, url_params_validation
 
 
-APP_NAME_LABEL = 'u
+APP_NAME_LABEL = 'user'
+
+
+def get_generic_serializer(name):
+    model_name = url_par
