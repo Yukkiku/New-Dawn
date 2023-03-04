3ca76c10cdb35a2f47fcb1d85782ffb1835135da
Yukkiku <106497277+Yukkiku@users.noreply.github.com>
2023-03-04 09:22:48 +0000

    Fix method

--- ecommerce/wsgi.py
+++ ecommerce/wsgi.py
@@ -3,4 +3,5 @@ WSGI config for ecommerce project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
-For mo
+For more information on this file, see
+https://docs.djangoproject.com/en/3.
