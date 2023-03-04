83b65c6b4a8fe55057f4dcc626d266309f32e77a
Yukkiku <106497277+Yukkiku@users.noreply.github.com>
2023-03-04 13:41:30 +0000

    Bump dcos-docs

--- ecommerce/wsgi.py
+++ ecommerce/wsgi.py
@@ -9,4 +9,6 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi impor
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJA
